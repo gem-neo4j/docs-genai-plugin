34096aa1cbea12d68e42c91ce34afe06b2be1998
Stefano Ottolenghi <stefano.ottolenghi@neo4j.com>
2025-02-12 09:53:28 +0100

    Initial push.

---
+++ server.js
@@ -0,0 +1,8 @@
+const express = require('express')
+
+const app = express()
+app.use(express.static('./build/site'))
+
+app.use('/static/assets', express.static('./build/site/assets'))
+
+app.listen(8000, () => console.log('📘 http://localhost:8000'))
